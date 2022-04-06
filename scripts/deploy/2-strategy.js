599f852caf6296e7fcf42ab4cdfcfc1a17853cf9
Magnus Brantheim <m.brantheim@gmail.com>
2022-04-06 09:50:32 +0200

    Deploy statera maxi

--- scripts/deploy/2-strategy.js
+++ scripts/deploy/2-strategy.js
@@ -1,7 +1,7 @@
 const hre = require('hardhat');
 
 async function main() {
-  const vaultAddress = 'TODO';
+  const vaultAddress = '0x796bC693dFa118220224F03a1F5a9eE7112d773e';
 
   const Strategy = await ethers.getContractFactory('ReaperStrategyBeethovenBeetsUnderlying');
   const treasuryAddress = '0x0e7c5313E9BB80b654734d9b7aB1FB01468deE3b';
@@ -11,6 +11,8 @@ async function main() {
   const strategist3 = '0x1A20D7A31e5B3Bc5f02c8A146EF6f394502a10c4';
   const wantAddress = '0x5Bb2DeE206C2bCc5768B9B2865eA971Bd9c0fF19';
   const mcPoolId = 52;
+
+  const options = {gasPrice: 130000000000, gasLimit: 9000000};
 
   const strategy = await hre.upgrades.deployProxy(
     Strategy,
@@ -22,6 +24,7 @@ async function main() {
       mcPoolId,
     ],
     {kind: 'uups', timeout: 0},
+    options,
   );
 
   await strategy.deployed();
--- scripts/deploy/3-initialize.js
+++ scripts/deploy/3-initialize.js
@@ -1,11 +1,12 @@
 async function main() {
-  const vaultAddress = 'TODO';
-  const strategyAddress = 'TODO';
+  const vaultAddress = '0x796bC693dFa118220224F03a1F5a9eE7112d773e';
+  const strategyAddress = '0xDb986d1E2b90AeeF43428F869aF8064F60143d44';
 
   const Vault = await ethers.getContractFactory('ReaperVaultv1_4');
   const vault = Vault.attach(vaultAddress);
+  const options = {gasPrice: 130000000000, gasLimit: 9000000};
 
-  await vault.initialize(strategyAddress);
+  await vault.initialize(strategyAddress, options);
   console.log('Vault initialized');
 }
 
