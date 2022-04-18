b1a431e54999d26638e9e845ce02597f8668f3a6
Magnus Brantheim <m.brantheim@gmail.com>
2022-04-18 15:19:04 +0200

    Rename strategy

--- test/starter-test.js
+++ test/starter-test.js
@@ -75,7 +75,7 @@ describe('Vaults', function () {
 
     //get artifacts
     Vault = await ethers.getContractFactory('ReaperVaultv1_4');
-    Strategy = await ethers.getContractFactory('ReaperStrategyBeethovenWftmUnderlying');
+    Strategy = await ethers.getContractFactory('ReaperStrategyBeethovenCre8r');
     Want = await ethers.getContractFactory('@openzeppelin/contracts/token/ERC20/ERC20.sol:ERC20');
 
     //deploy contracts
