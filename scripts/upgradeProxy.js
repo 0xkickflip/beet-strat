2cba78eef961ab9cd354556caa23c9e43e1561fc
tess3rac7 <tess3rac7@protonmail.com>
2022-04-20 14:26:28 -0400

    Post-deployment commit.

--- scripts/upgradeProxy.js
+++ scripts/upgradeProxy.js
@@ -1,7 +1,6 @@
 async function main() {
-  const stratFactory = await ethers.getContractFactory('ReaperAutoCompoundSolidexFarmer');
-  const stratContract = await hre.upgrades.upgradeProxy('0x3630a380F320EA77284Ed03D09B4C73D1351C41e', stratFactory, {
-    call: {fn: 'postUpgradeLP0Allowance'},
+  const stratFactory = await ethers.getContractFactory('ReaperStrategyBeethovenWftmUnderlying');
+  const stratContract = await hre.upgrades.upgradeProxy('0xa9ca7185c0D3D71dF19C71Af6Bb007E2a773BFfD', stratFactory, {
     timeout: 0,
   });
   console.log('Strategy upgraded!');
