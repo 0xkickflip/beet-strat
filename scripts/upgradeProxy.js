c019099a98b2393868fcab858afbef1732e860d9
Magnus Brantheim <m.brantheim@gmail.com>
2022-05-01 09:46:11 +0200

    Upgraded strategy to handle FHM rewards

--- scripts/upgradeProxy.js
+++ scripts/upgradeProxy.js
@@ -1,10 +1,19 @@
 async function main() {
-  const stratFactory = await ethers.getContractFactory('ReaperAutoCompoundSolidexFarmer');
-  const stratContract = await hre.upgrades.upgradeProxy('0x3630a380F320EA77284Ed03D09B4C73D1351C41e', stratFactory, {
-    call: {fn: 'postUpgradeLP0Allowance'},
+  const stratFactory = await ethers.getContractFactory('ReaperStrategyBeethovenDaiUnderlying');
+  const strategyAddress = '0xD94278BE8Fe39Dfb5449204092024ef541187beF';
+  await hre.upgrades.upgradeProxy(strategyAddress, stratFactory, {
     timeout: 0,
   });
   console.log('Strategy upgraded!');
+  const strategy = stratFactory.attach(strategyAddress);
+  await strategy.setPathsPostUpgrade(
+    [
+      '0xfa1FBb8Ef55A4855E5688C0eE13aC3f202486286',
+      '0x8D11eC38a3EB5E956B052f67Da8Bdc9bef8Abf3E',
+      '0x21be370D5312f44cB42ce377BC9b8a0cEF1A4C83',
+    ],
+    ['0xfa1FBb8Ef55A4855E5688C0eE13aC3f202486286', '0x8D11eC38a3EB5E956B052f67Da8Bdc9bef8Abf3E'],
+  );
 }
 
 main()
