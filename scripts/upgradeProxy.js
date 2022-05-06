9a20b7c709e1ce09b4e1123af3aba585876ed86c
tess3rac7 <tess3rac7@protonmail.com>
2022-05-06 17:22:12 -0400

    Post deployment commit.

--- scripts/upgradeProxy.js
+++ scripts/upgradeProxy.js
@@ -1,10 +1,16 @@
 async function main() {
-  const stratFactory = await ethers.getContractFactory('ReaperAutoCompoundSolidexFarmer');
-  const stratContract = await hre.upgrades.upgradeProxy('0x3630a380F320EA77284Ed03D09B4C73D1351C41e', stratFactory, {
-    call: {fn: 'postUpgradeLP0Allowance'},
-    timeout: 0,
-  });
-  console.log('Strategy upgraded!');
+  const stratFactory = await ethers.getContractFactory('ReaperStrategyTwoGodsOnePool');
+  // const stratContract = await hre.upgrades.upgradeProxy('0x0310b9979BcC17fa2DB4cEC4417FCebabc405F1D', stratFactory);
+  // console.log('Strategy upgraded!');
+  const stratContract = await stratFactory.attach('0x0310b9979BcC17fa2DB4cEC4417FCebabc405F1D');
+
+  const daiAddress = '0x8D11eC38a3EB5E956B052f67Da8Bdc9bef8Abf3E';
+  const deusAddress = '0xDE5ed76E7c05eC5e4572CfC88d1ACEA165109E44';
+
+  // await stratContract.pause();
+  // await stratContract.addSwapStep(deusAddress, daiAddress, 1 /* total fee */, 0);
+  // await stratContract.addChargeFeesStep(daiAddress, 0 /* absolute */, 10_000);
+  // await stratContract.unpause();
 }
 
 main()
