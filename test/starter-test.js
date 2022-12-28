b98e5df5c2a1b94518f849eab65bcc53d64693e5
imrtlfarm <imrtlfarm@gmail.com>
2022-12-28 13:32:54 -0800

    more changes

--- test/starter-test.js
+++ test/starter-test.js
@@ -29,11 +29,11 @@ const strategistAddr = '0x1A20D7A31e5B3Bc5f02c8A146EF6f394502a10c4';
 const strategists = [strategistAddr];
 const multisigRoles = [superAdminAddress, adminAddress, guardianAddress];
 
-const wantAddress = '0xB0de49429fBb80c635432bbAD0B3965b28560177';
-const gauge = '0x3CE5dD8D3C2DF2bb599A94523509004d2af17516';
+const wantAddress = '0x428E1CC3099cF461B87D124957A0d48273f334b1';
+const gauge = '0x2d801513d36aA8aA2579133507462f0DE05a40Ff';
 const usdcAddress = '0x7F5c764cBc14f9669B88837ca1490cCa17c31607';
 
-const wantHolderAddr = '0x3b410908e71Ee04e7dE2a87f8F9003AFe6c1c7cE';
+const wantHolderAddr = '0x052463c5b558F273B69F5d831603A815aE4eea41';
 
 describe('Vaults', function () {
   async function deployVaultAndStrategyAndGetSigners() {
@@ -63,7 +63,7 @@ describe('Vaults', function () {
     const Want = await ethers.getContractFactory('@openzeppelin/contracts/token/ERC20/ERC20.sol:ERC20');
 
     // deploy contracts
-    const vault = await Vault.deploy(wantAddress, `Happy Road Reloaded Beethoven-X Crypt`, 'rf-bb-HAPPY', 0, ethers.constants.MaxUint256);
+    const vault = await Vault.deploy(wantAddress, `Here Comes the Sonne Beethoven-X Crypt`, 'rf-bb-soUSD', 0, ethers.constants.MaxUint256);
     await vault.deployed();
     const strategy = await upgrades.deployProxy(
       Strategy,
